26a23b1786d8dcd865bd4057e7356a602cd76da6
WhiteHeadbanger <white.seba@hotmail.com>
2021-11-15 00:11:47 -0300

    Drag and draw initial implementation
    Lags a bit and not quite accurate when rapid dragging, but it is usable

--- app.py
+++ app.py
@@ -8,15 +8,19 @@ import sys
 
 class Pyxel:
 
-    def __init__(self, tile_size: int, canvas_width: int = None, canvas_height: int = None):
+    def __init__(self, tile_size: int):
         pg.init()
+        # SCREENS #
+        # Main screen
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        #
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
         pg.key.set_repeat(500, 100)
         self.load_data()
-        self.canvas_width = canvas_width
-        self.canvas_height = canvas_height
+        self.draw_screen_w = WIDTH - int((WIDTH * 0.2))
+        self.draw_screen_h = HEIGHT 
+        
         self.tile_size = tile_size
 
     def load_data(self):
@@ -34,24 +38,27 @@ class Pyxel:
         self.gui = GUI(self)
         self.load_toolbtns()
         self.canvas = []
-        for x in range(0, int(WIDTH / self.tile_size) if self.canvas_width == None else int(self.canvas_width / self.tile_size)):
+        for x in range(0, self.tile_size * self.tile_size):
             self.canvas.append([])
-            for y in range(0, int(WIDTH / self.tile_size) if self.canvas_height == None else int(self.canvas_height / self.tile_size)):
+            for y in range(0, self.tile_size * self.tile_size):
                 self.canvas[x].append(0)
         
 
     def draw_grid(self):
-        for x in range(0, WIDTH if self.canvas_width == None else self.canvas_width, self.tile_size):
-            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT if self.canvas_height == None else self.canvas_height))
-        for y in range(0, HEIGHT if self.canvas_height == None else self.canvas_height, self.tile_size):
-            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH if self.canvas_width == None else self.canvas_width, y))
+        for x in range(0, WIDTH if self.draw_screen_w == None else self.draw_screen_w, self.tile_size):
+            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT if self.draw_screen_h == None else self.draw_screen_h))
+        for y in range(0, HEIGHT if self.draw_screen_h == None else self.draw_screen_h, self.tile_size):
+            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH if self.draw_screen_w == None else self.draw_screen_w, y))
 
     def load_toolbtns(self):
-        x = WIDTH - int(WIDTH * 0.2) + 5
-        self.pencil_tool = ToolBtns(self, self.pencil_icon, self.pencil_icon_hover, x, 5)
-        self.erase_tool = ToolBtns(self, self.pencil_icon, self.pencil_icon_hover, x + 37, 5)
-        self.rectangle_tool = ToolBtns(self, self.pencil_icon, self.pencil_icon_hover, x + 37 + 37, 5)
-        self.circle_tool = ToolBtns(self, self.pencil_icon, self.pencil_icon_hover, x + 37 + 37 + 37, 5)
+        w = WIDTH - int(WIDTH * 0.2) + 5
+        self.pencil_tool = ToolBtns(self, self.pencil_icon, self.pencil_icon_hover, w, 5)
+        w += 37
+        self.erase_tool = ToolBtns(self, self.pencil_icon, self.pencil_icon_hover, w, 5)
+        w += 37
+        self.rectangle_tool = ToolBtns(self, self.pencil_icon, self.pencil_icon_hover, w, 5)
+        w += 37
+        self.circle_tool = ToolBtns(self, self.pencil_icon, self.pencil_icon_hover, w, 5)
 
     def draw_toolbtns(self):
         self.pencil_tool.draw()
@@ -63,7 +70,7 @@ class Pyxel:
         """ App loop """
         self.running = True
         while self.running:
-            self.dt = self.clock.tick(FPS)
+            self.dt = self.clock.tick()
             self.draw()
             self.events()
 
@@ -74,34 +81,63 @@ class Pyxel:
     def events(self):
         # catch all events here
         for event in pg.event.get():
+            mouse_state = pg.mouse.get_pressed()
+            x, y = self.get_mouse_tile()
             if event.type == pg.QUIT:
                 self.quit()
             elif event.type == pg.MOUSEBUTTONDOWN:
-                x = event.pos[0] // self.tile_size
-                y = event.pos[1] // self.tile_size
-                print(x, y)
-                self.register_pixel(x, y, exists=self.exists(x, y))
+                # Left click
+                if event.button == 1: #and self.selected_tool == Tools.Pencil:
+                    print(x, y)
+                    self.register_pixel(x, y, color=BLACK)
+                # Right click
+                if event.button == 3: #and self.selected_tool == Tools.Pencil:
+                    print(x, y)
+                    self.register_pixel(x, y, color=RED)
+            elif event.type == pg.MOUSEMOTION:
+                # Left click
+                if mouse_state[0]: #and self.selected_tool == Tools.Pencil:
+                    print(x, y)
+                    self.register_pixel(x, y, color=BLACK)
+                # Right click
+                if mouse_state[2]: #and self.selected_tool == Tools.Pencil:
+                    print(x, y)
+                    self.register_pixel(x, y, color=RED)
+
+            # Hover on tool buttons
             for toolbtns in self.tool_buttons:
                 if toolbtns.rect.collidepoint(pg.mouse.get_pos()):
                     toolbtns.hovered = True
                 else:
                     toolbtns.hovered = False
 
-    def register_pixel(self, x, y, exists = False):
+    def get_mouse_tile(self):
+        """ Returns the tile that the mouse is pointing """
+
+        mouse_pos = pg.Vector2(pg.mouse.get_pos())
+        x, y = [int(v // self.tile_size) for v in mouse_pos]
+        if x >= 0 and y >= 0:
+            return (x, y)
+
+    def register_pixel(self, x, y, color):
         """ Register new pixels onto the canvas matrix """
 
-        if not exists:
+        if not self.exists(x, y):
             try:
-                self.canvas[x][y] = 1
+                self.canvas[x][y] = {"color":color, "status":1}
                 return
             except IndexError:
                 print("Fuera de rango")  
+    
+    def unregister_pixel(self, x, y):
+        """ Unregister a pixel from the canvas matrix """
+
+        if self.exists(x, y):
+            try:
+                self.canvas[x][y] = 0
+            except IndexError:
+                print("Fuera de rango")
         
-        try:
-            self.canvas[x][y] = 0
-            return
-        except IndexError:
-            print("Fuera de rango")
 
 
     def exists(self, x, y):
@@ -118,23 +154,23 @@ class Pyxel:
 
         for row, data in enumerate(self.canvas):
             for col, px in enumerate(data):
-                if px == 1:
-                    pg.draw.rect(self.screen, BLACK, pg.Rect(row * self.tile_size, col * self.tile_size, self.tile_size, self.tile_size))
+                if px:
+                    if px["status"] == 1:
+                        pg.draw.rect(self.screen, px["color"], pg.Rect(row * self.tile_size, col * self.tile_size, self.tile_size, self.tile_size))
 
     def draw(self):
         self.screen.fill(WHITE)
         self.draw_grid()
+        self.draw_pixel()
         self.gui.draw()
         self.draw_toolbtns()
-        self.draw_pixel()
         pg.display.flip()
 
 if __name__ == "__main__":
 
-    w = int(input("Canvas Width >> "))
-    h = int(input("Canvas Height >> "))
+    
     tile = int(input("Tile Size >> "))
-    p = Pyxel(tile, w, h)
+    p = Pyxel(tile)
     p.new()
     while True:
         p.run()
